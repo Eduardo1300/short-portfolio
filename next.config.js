c19b4352b15acfab7fd00caf099dd5409492e476
Eduardo Valdivia <eduardovaldivia130@outlook.es>
2026-01-31 23:13:31 -0500

    Add rewrites and asset resource handling in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: '/:locale/fonts/:path*',
+          destination: '/fonts/:path*',
+        },
+      ],
+    };
+  },
+  webpack: (config, { isServer }) => {
+    config.module.rules.push({
+      test: /\.(woff|woff2|eot|ttf|otf)$/,
+      type: 'asset/resource',
+    });
+    return config;
+  },
 }
 
 module.exports = nextConfig
